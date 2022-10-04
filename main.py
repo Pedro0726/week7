5163d10a054082ce7d2b8682d311ccbd76dc22ac
Pedro Almazan-Guzman <113930479+Pedro0726@users.noreply.github.com>
2022-10-04 17:25:27 +0000

    loops

---
+++ main.py
@@ -0,0 +1,36 @@
+# fork the repl.it and create a new git hub repo entitled
+#Comparison operators
+# Logical operators
+# Decision making
+# loops(for loops, while loops, range, enumerator)
+# min/max practice
+# Random in python
+# List comprehension
+#importing modules
+#modularized ypur code
+#work on creating and understanding functions
+
+# from decision_making import decisions
+# from comparison_operators import comparison
+from loops import loop
+# from enumerators import enumratrs
+# from list_comprehension import listComprehension
+# from logical_operators import logicalOperators
+# from min_max import minMax 
+# from random import randm 
+# from ranges import rangs
+# from while_loops import whileLoops 
+# from zip import zp
+# from dictionaries_loops import dictionaries_loops
+
+# decisions()
+# comparison()
+loop()
+# enumratrs()
+# listComprehension()
+# logicalOperators()
+# minMax()
+# randm()
+# rangs()
+# whileLoops()
+# zp()
